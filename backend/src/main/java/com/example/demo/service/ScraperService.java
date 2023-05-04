5d38a143d3faa1bc1c3832782331b22ab31853ed
kevinlee584 <45327214+kevinlee584@users.noreply.github.com>
2023-05-04 23:06:58 +0800

    add cache layer

--- backend/src/main/java/com/example/demo/service/ScraperService.java
+++ backend/src/main/java/com/example/demo/service/ScraperService.java
@@ -8,40 +8,57 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+@AllArgsConstructor
 
+class Tuple<X, Y> {
+    public final X x;
+    public final Y y;
+}
 @Service
 public class ScraperService {
 
     private final ChromeOptions options;
     private ChromeDriver driver ;
+    private final Map<String, Tuple<Instant, List<Drama>>> dramasCache =  new HashMap<>();
 
     public ScraperService(@Autowired ChromeOptions options) {
         this.options = options;
-    }
-
-    @PostConstruct
-    void postConstruct() {
         driver = new ChromeDriver(options);
     }
 
-    synchronized public List<Drama> scrape(Function<ChromeDriver, List<Drama>> scraper) {
-        WebDriverException error = null;
+    synchronized public List<Drama> scrape(String url, Function<ChromeDriver, List<Drama>> scraper) {
 
-        for (int i = 0; i < 2; ++i) {
-            try {
-                return scraper.apply(driver);
-            }catch (WebDriverException e) {
-                error = e;
-                driver.quit();
-                driver = new ChromeDriver(options);
+        var list = dramasCache.get(url);
+
+        if (list == null || Duration.between(Instant.now(), list.x).toMillis() > 300_000) {
+
+            WebDriverException error = null;
+
+            for (int i=0; i<2; ++i) {
+                try {
+                    var result =scraper.apply(driver);
+                    dramasCache.put(url, new Tuple<>(Instant.now(), result));
+                    return result;
+
+                }catch (WebDriverException e) {
+                    error = e;
+                    driver.quit();
+                    driver = new ChromeDriver(options);
+                }
             }
+
+            throw error;
         }
 
-        throw error;
+        return list.y;
+
     }
 
     @PreDestroy
