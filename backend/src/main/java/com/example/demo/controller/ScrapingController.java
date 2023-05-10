44b731e598a133f017222d1ab4090ada3baaae98
kevinlee584 <45327214+kevinlee584@users.noreply.github.com>
2023-05-10 10:55:57 +0800

    modify ScrapingController and ScraperService

--- backend/src/main/java/com/example/demo/controller/ScrapingController.java
+++ backend/src/main/java/com/example/demo/controller/ScrapingController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Drama;
-import com.example.demo.scraping.ScraperScripts;
 import com.example.demo.service.ScraperService;
-import lombok.AllArgsConstructor;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -18,31 +18,37 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@AllArgsConstructor
 public class ScrapingController {
 
     final private ScraperService scraperService;
+    private List<Map<String, Object>> providers;
 
-    private List<ProvidersResponseTemplate> getProviders() {
-        return ScraperScripts.scrapers.entrySet().stream()
+    public ScrapingController(
+            @Autowired ScraperService scraperService
+    ){
+        this.scraperService = scraperService;
+
+        providers = scraperService.getAllScraper().entrySet().stream()
                 .map(e -> {
                     String providerName = e.getKey();
                     String providerFavoriteIcon = e.getValue().getProvider().getFaviconUrl();
                     Map<String, String> providerSorts =  e.getValue().getScripts().keySet().stream()
                             .collect(Collectors.toMap(e2 -> e2, e2 ->"/provider/" + providerName + "/" + e2));
-                    return new ProvidersResponseTemplate(providerName, providerFavoriteIcon, providerSorts);
+                    return Map.of("provider", providerName,
+                            "favicon", providerFavoriteIcon,
+                            "sorts", providerSorts);
                 }).collect(Collectors.toList());
     }
 
     @GetMapping("/providers")
-    public List<ProvidersResponseTemplate> getAllProviders(){
-        return getProviders();
+    public List<Map<String, Object>> getAllProviders(){
+        return providers;
     }
 
     @GetMapping("/provider/{provider}/{sort}")
     public ResponseEntity<?> getDramas(@PathVariable("provider") String provider, @PathVariable("sort") String sort){
 
-        Optional<Function<ChromeDriver, List<Drama>>> result = ScraperScripts.scrapers.entrySet().stream()
+        Optional<Function<ChromeDriver, List<Drama>>> result = scraperService.getAllScraper().entrySet().stream()
                 .filter(e -> e.getKey().equals(provider))
                 .map(e -> e.getValue().getScripts().get(sort))
                 .filter(Objects::nonNull)
--- backend/src/main/java/com/example/demo/service/ScraperService.java
+++ backend/src/main/java/com/example/demo/service/ScraperService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Drama;
-import lombok.AllArgsConstructor;
+import com.example.demo.scraping.Scraper;
+import com.example.demo.scraping.ScraperScripts;
+import com.example.demo.utils.Tuple;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,31 +11,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
-@AllArgsConstructor
-
-class Tuple<X, Y> {
-    public final X x;
-    public final Y y;
-}
 @Service
 public class ScraperService {
 
     private final ChromeOptions options;
     private ChromeDriver driver ;
-
-    @Value("${webScrape.cache.expires}")
     private long expire;
     private final Map<String, Tuple<Instant, List<Drama>>> dramasCache =  new HashMap<>();
 
-    public ScraperService(@Autowired ChromeOptions options) {
+    public ScraperService(
+            @Value("${webScrape.cache.expires}") long expire,
+            @Autowired ChromeOptions options
+    ) {
+        this.expire = expire;
         this.options = options;
         driver = new ChromeDriver(options);
     }
@@ -64,6 +62,21 @@ public class ScraperService {
 
         return list.y;
 
+    }
+
+    public Map<String, Scraper> getAllScraper() {
+        return ScraperScripts.scrapers;
+    }
+
+    public Drama getDrama(String provider, String drama) {
+
+        Optional<Drama> result =  dramasCache.entrySet().stream()
+                .filter(e -> e.getKey().startsWith(String.format("/provider/%s", provider)))
+                .flatMap(e -> e.getValue().y.stream())
+                .filter(e -> e.getName().equals(drama))
+                .findFirst();
+
+        return result.orElse(null);
     }
 
     @PreDestroy
