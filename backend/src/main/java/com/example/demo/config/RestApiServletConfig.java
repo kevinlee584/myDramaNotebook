8207ead1b87a62743ba1a797466bd5ea3307278d
kevinlee584 <45327214+kevinlee584@users.noreply.github.com>
2023-05-11 08:51:12 +0800

    add browser preflight config

---
+++ backend/src/main/java/com/example/demo/config/RestApiServletConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class RestApiServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+    }
+}
