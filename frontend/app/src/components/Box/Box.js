373a2e55d0a15e92ee16179bd2ceb858c2ec64b4
kevinlee584 <45327214+kevinlee584@users.noreply.github.com>
2023-05-21 12:31:14 +0800

    modify some details

--- frontend/app/src/components/Box/Box.js
+++ frontend/app/src/components/Box/Box.js
@@ -38,71 +38,13 @@ const Box = ({ drama, record }) => {
                         <p>{name}</p>
                     </div>
                 </div>
-                <div className="save-botton" onClick={saveFun}>
+                <button className="save-button" onClick={saveFun}>
                     { isSaved ? <Saved></Saved> : <Save></Save> }
-                </div>
+                </button>
             </>
         </a>
     </div>
-)
-
-
-
-
-
-
-
-
-
-
-    // const { providerName, imageUrl, videoUrl, name } = drama
-    // const isSave = record.find(e => e.providerName == providerName && e.name == name) != undefined
-    // const [saved, setSaved] = useState(isSave)
-
-    // const clicked = (event) => {
-    //     event.preventDefault()
-    //     if (saved) {
-    //         fetch(`${config.backend.url}/user/remove`, 
-    //             {method: "DELETE", headers: headers, 
-    //             body: JSON.stringify({provider: providerName, name: name})})
-    //         .then(res => {
-    //             setSaved(false)
-    //             let index = record.findIndex(e => e.providerName == providerName && e.name == name)
-    //             record.splice(index, 1)
-    //             setRecord(record)
-    //         })
-    //     }
-    //     else {
-    //         fetch(`${config.backend.url}/user/save`, 
-    //             {method: "POST", 
-    //             headers: headers, 
-    //             body: JSON.stringify({provider: providerName, name: name})})
-    //         .then(res => {
-    //             setSaved(true)
-    //             record.push(drama)
-    //             setRecord(record)
-    //         })
-    //     }
-    // }
-
-    // return (
-    //     <div className="box">
-    //         <a href={videoUrl} className="video-block" target="_blank">
-    //             <>
-    //                 <img src={imageUrl} className="video-image"></img>
-    //                 <div className="video-name-blocker">
-    //                     <div className="video-name">
-    //                         <p>{name}</p>
-    //                         <p>{name}</p>
-    //                     </div>
-    //                 </div>
-    //                 <div className="save-botton" onClick={clicked}>
-    //                     { saved ? <Saved></Saved> : <Save></Save> }
-    //                 </div>
-    //             </>
-    //         </a>
-    //     </div>
-    // )
+    )
 };
 
 export default Box;
--- frontend/app/src/components/Menu/Menu.js
+++ frontend/app/src/components/Menu/Menu.js
@@ -14,16 +14,16 @@ const Menu = function({providers, showMenu, setShowMenu, setPage}) {
 
     const menu_blockers = []
     for(const i in providers) {
-        var e = <div className='provider-block' key={providers[i].provider} onClick={() => setSelectedProvider(selectedProvider == i ? -1 : i)}>
+        var e = <li className='provider-block' key={providers[i].provider} onClick={() => setSelectedProvider(selectedProvider == i ? -1 : i)}>
                     <img src={providers[i].favicon} className='provider-icon'></img>
-                </div>
+                </li>
 
         menu_blockers.push(e)
     }
 
-    const record = <div className='provider-block' key={"recordProvider"} onClick={() => {setPage("record"); setShowMenu(false);}}>
+    const record = <li className='provider-block' key={"recordProvider"} onClick={() => {setPage("record"); setShowMenu(false);}}>
                         <VideoesImage></VideoesImage>
-                    </div>
+                    </li>
 
     menu_blockers.push(record)
 
@@ -33,16 +33,16 @@ const Menu = function({providers, showMenu, setShowMenu, setPage}) {
 
         const sorts_blockers = Object.entries(providers[i].sorts)
             .map(e => (
-                <div className='sort-blocker' key={`${providers[i].provider}/${e[0]}`} onClick={() => {setPage(e[1]); setShowMenu(false); }}>
+                <li className='sort-blocker' key={`${providers[i].provider}/${e[0]}`} onClick={() => {setPage(e[1]); setShowMenu(false); }}>
                     <p>{ e[0] }</p>
-                </div>))
+                </li>))
 
-        sorts_block = <div className='sort-block'>{ sorts_blockers }</div>
+        sorts_block = <ul className='sort-block'>{ sorts_blockers }</ul>
     }
 
     return (
         <div id='menu'>
-            <div className='menu-block'>{ menu_blockers }</div>
+            <ul className='menu-block'>{ menu_blockers }</ul>
             { sorts_block  }
             <div className='background-blur-layer' onClick={() => {setShowMenu(false)}}></div>
         </div>
