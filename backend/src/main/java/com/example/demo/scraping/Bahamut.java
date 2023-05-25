12c9609d44bfc97b13bdfa8124656d8716abe2f5
kevinlee584 <45327214+kevinlee584@users.noreply.github.com>
2023-05-25 09:04:32 +0800

    use JSoup to speed up parser

--- backend/src/main/java/com/example/demo/scraping/Bahamut.java
+++ backend/src/main/java/com/example/demo/scraping/Bahamut.java
@@ -3,6 +3,9 @@ package com.example.demo.scraping;
 
 import com.example.demo.model.Drama;
 import com.example.demo.model.Provider;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,6 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -33,20 +37,23 @@ final public class Bahamut implements Scraper{
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("anime-block")));
 
-        final WebElement words = driver.findElement(By.className("newanime-block"));
-        final List<WebElement> animates = words.findElements(By.className("anime-block"));
+        final WebElement block = driver.findElement(By.className("newanime-block"));
+
+        Document document = Jsoup.parse(block.getAttribute("innerHTML"));
+        Elements animates = document.select(".anime-block");
+
 
         return animates.stream().map(e -> {
-            String animeName = e.findElement(By.className("anime-name"))
-                    .findElement(By.tagName("p")).getAttribute("textContent");
-            String animePicUrl = e.findElement(By.className("anime-blocker"))
-                    .findElement(By.tagName("img")).getAttribute("data-src");
-            String animeVideoUrl = e.findElement(By.className("anime-card-block"))
-                    .getAttribute("href");
+            try {
+                String animeName = e.selectFirst(".anime-name > p").attr("textContent");
+                String animePicUrl = e.selectFirst(".anime-blocker > img").attr("data-src");
+                String animeVideoUrl = e.selectFirst(".anime-card-block").attr("href");
 
-
-            return new Drama("bahamut", animeName, animePicUrl, animeVideoUrl);
-        }).collect(Collectors.toList());
+                return new Drama("bahamut", animeName, animePicUrl, animeVideoUrl);
+            } catch (NullPointerException error) {
+                return null;
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     private List<Drama> getHotDramas(ChromeDriver driver) {
@@ -55,18 +62,21 @@ final public class Bahamut implements Scraper{
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("blockHotAnime")));
 
-        final WebElement words = driver.findElement(By.id("blockHotAnime")).findElement(By.className("theme-list-block"));
-        final List<WebElement> animates = words.findElements(By.className("theme-list-main"));
+        final WebElement block = driver.findElement(By.id("blockHotAnime")).findElement(By.className("theme-list-block"));
+        Document document = Jsoup.parse(block.getAttribute("innerHTML"));
+        Elements animates = document.select(".theme-list-main");
 
         return animates.stream().map(e -> {
-            String animeName = e.findElement(By.className("theme-info-block"))
-                    .findElement(By.tagName("p")).getAttribute("textContent");
-            String animePicUrl = e.findElement(By.tagName("img")).getAttribute("data-src");
-            String animeVideoUrl = e.getAttribute("href");
+            try {
+                String animeName = e.selectFirst(".theme-info-block > p").attr("textContent");
+                String animePicUrl = e.selectFirst("img").attr("data-src");
+                String animeVideoUrl = e.attr("href");
 
-
-            return new Drama("bahamut", animeName, animePicUrl, animeVideoUrl);
-        }).collect(Collectors.toList());
+                return new Drama("bahamut", animeName, animePicUrl, animeVideoUrl);
+            }catch (NullPointerException error) {
+                return null;
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     @Override
