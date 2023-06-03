c98d6f411ca142bd2f83e72008bf24f89c994c7f
kevinlee584 <45327214+kevinlee584@users.noreply.github.com>
2023-06-04 03:46:59 +0800

    add volumes to save record

--- backend/src/main/java/com/example/demo/repo/RecordRepository.java
+++ backend/src/main/java/com/example/demo/repo/RecordRepository.java
@@ -1,35 +1,22 @@
 package com.example.demo.repo;
 
 import com.example.demo.model.Drama;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 public class RecordRepository {
     private List<Drama> record = new ArrayList<>();
-    private String fileLocation = "dramasRecord.dat";
+    final private String fileLocation = "/dramasRecord.dat";
 
     public RecordRepository() {
-
-        Logger logger = LoggerFactory.getLogger(RecordRepository.class);
-
-        if (Objects.nonNull(System.getProperty("user.home"))) {
-            fileLocation = Paths.get(System.getProperty("user.home"), fileLocation).toString();
-        }
-
-        logger.info("FileLocation is " + fileLocation);
-
         try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(fileLocation))){
             record = (List<Drama>) input.readObject();
         }catch (Exception ignore){}
